cb5b650124447709bbc3f55dd6ecba2ec01668eb
AN-Fahmy <anfahmy92@gmail.com>
2024-09-22 13:12:32 +0300

    myPortfolio

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* Type Js Home Section */
+var typed = new Typed('.text-animated span', {
+    strings: ['Front-End Developer','Angular Developer' , 'Web Designer'],
+    typeSpeed: 40,
+    backSpeed: 50,
+    loop: true,
+});
+
